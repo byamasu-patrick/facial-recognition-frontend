730741c18bc9ef7744d7ede263cbeaa5d8bb8159
Byamasu Patrick Paul <ptrckbyamasu@gmail.com>
2022-09-27 15:15:47 +0200

    Updated the repo

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
         <>
-        <footer class="bg-white pb-6 py-6">    
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" class="hover:underline">Ntibu.com™</a>. All Rights Reserved.
+        <footer className="bg-white pb-6 py-6">    
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" className="hover:underline">Ntibu.com™</a>. All Rights Reserved.
             </span>
         </footer>   
         </>
